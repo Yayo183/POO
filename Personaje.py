a4094049832c9c87c4786fdb32048375c5bb7047
Yayo183 <122404179+Yayo183@users.noreply.github.com>
2023-02-20 16:59:37 -0600

    Se crearon los metodos para el personaje

--- Personaje.py
+++ Personaje.py
@@ -4,3 +4,20 @@ class Personaje:
     especie = "Humano"
     Nombre = "Master Chief"
     Alturas = "2.70"
+    
+    #Metodo
+    def correr(self,status):
+        if(status):
+            print("El personaje " + self.Nombre + "esta corriendo")
+        else:
+            print("El personaje " + self.Nombre + "Se detuvo")
+    
+    def LanzarGranadas(self):
+        print("El personaje " + self.Nombre + "lanzo granada a un covenant")
+        
+    def RecargarArma(self,municiones):
+        cargador = 10
+        cargador = cargador + municiones
+        print("El arma tiene" + cargador + "balas")
+            
+        
